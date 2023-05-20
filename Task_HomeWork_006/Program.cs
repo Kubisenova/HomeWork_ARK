602340d19a7851caa446ef3828c5543d067cc470
Aygul <Your>
2023-05-21 01:04:50 +0300

    Task_HomeWork_006

--- Task_HomeWork_006/Program.cs
+++ Task_HomeWork_006/Program.cs
@@ -4,13 +4,28 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
-Console.WriteLine("Введите число: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-if (numberA %2 == 0)
+// Console.WriteLine("Введите число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// if (numberA %2 == 0)
+// {
+//   Console.WriteLine("Число чётное ");  
+// }
+// else
+// {
+//     Console.WriteLine("Число нечётное "); 
+// }
+
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+string text = Console.ReadLine();
+int number = Convert.ToInt32(text);
+
+if (number % 2 == 0)
 {
-  Console.WriteLine("Число чётное ");  
+Console.WriteLine("Число чётное ");
 }
 else
 {
-    Console.WriteLine("Число нечётное "); 
+    Console.WriteLine("Число НЕ чётное ");
 }
