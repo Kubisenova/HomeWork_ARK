354034f90d67ff84a26b95fe6c9e35cd48f17d02
Aygul <Your>
2023-05-21 00:26:13 +0300

    Task_HomeWork_002

--- Task_HomeWork_002/Program.cs
+++ Task_HomeWork_002/Program.cs
@@ -5,15 +5,34 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите первое число; ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число; ");
-int numberB = Convert.ToInt32(Console.ReadLine());
-if(numberA > numberB)
+// Console.WriteLine("Введите первое число; ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число; ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// if(numberA > numberB)
+// {
+//     Console.WriteLine("Первое число больше второго");
+// }
+// else
+// {
+//     Console.WriteLine("Второе число больше первого");
+// }
+
+Console.WriteLine("Введите первое число ");
+string text1 = Console.ReadLine();
+int numberA = Convert.ToInt32(text1);
+
+Console.WriteLine("Введите второе число ");
+string text2 = Console.ReadLine();
+int numberB = Convert.ToInt32(text2);
+
+if (numberA > numberB)
 {
-    Console.WriteLine("Первое число больше второго");
+    Console.WriteLine("Первое число " + numberA + " больше второго числа " + numberB);
 }
 else
 {
-    Console.WriteLine("Второе число больше первого");
+    Console.WriteLine("Второе число " + numberB + " больше первого числа " + numberA);
 }
+
+
