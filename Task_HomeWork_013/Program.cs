8b7583c89afbdeb19826447ee3e5b29c473d7ff3
Aygul <Your>
2023-05-22 19:54:25 +0300

    Task_HomeWork_015

--- Task_HomeWork_013/Program.cs
+++ Task_HomeWork_013/Program.cs
@@ -55,64 +55,64 @@
 // Задача 13: Напишите программу, которая выводит третью
 // цифру заданного числа или сообщает, что третьей цифры нет.
 
-// Console.Clear();
-
-// Console.WriteLine("Введите число ");
-// int size = Convert.ToInt32(Console.ReadLine());
-// int number1 = size.ToString().Length;
-
-// if (number1 >= 4 || number1 <= 3)
-// {
-//     Console.WriteLine(" Это третья цифра " + number1);
-// }
-// else
-// {
-//     Console.WriteLine(" Здесь нет третьей цифры");
-// }
-
-
-
-// Решение 3
-
 Console.Clear();
 
-int number = ReadInt("Введите число: ");
-int count = number.ToString().Length;
-Console.Write(MakeArray(number, count));
+Console.WriteLine("Введите число ");
+int size = Convert.ToInt32(Console.ReadLine());
+int number1 = size.ToString().Length;
 
-// ФУНКЦИИ-------------------------------
-// Функция принимает сообщение для отображения 
-// в консоли, и выводит результат введенных данных пользователем.
-
-int ReadInt(string message)
+if (number1 >= 4 || number1 <= 3)
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(" Это третья цифра " + number1);
+}
+else
+{
+    Console.WriteLine(" Здесь нет третьей цифры");
 }
 
-// Функция принимает число введенное пользователем, 
-// количество символов, и выводит третью цифру числа. 
-// Если 3 цифры нет, сообщает и выводит 0.
 
-int MakeArray(int a, int b)
-{
-int result = 0;
-    if (b < 3)
-    {
-        Console.Write("Третьей цифры нет, держи: ");
-    }
-    else
-    {
-        int c = 1;
-        for (int i = b; i > 3; i--)
-        {
-            c = c * 10;
-        }
 
-        result = (a / c) % 10;
-    }
-return result;
-}
+// // Решение 3
+
+// Console.Clear();
+
+// int number = ReadInt("Введите число: ");
+// int count = number.ToString().Length;
+// Console.Write(MakeArray(number, count));
+
+// // ФУНКЦИИ-------------------------------
+// // Функция принимает сообщение для отображения 
+// // в консоли, и выводит результат введенных данных пользователем.
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// // Функция принимает число введенное пользователем, 
+// // количество символов, и выводит третью цифру числа. 
+// // Если 3 цифры нет, сообщает и выводит 0.
+
+// int MakeArray(int a, int b)
+// {
+// int result = 0;
+//     if (b < 3)
+//     {
+//         Console.Write("Третьей цифры нет, держи: ");
+//     }
+//     else
+//     {
+//         int c = 1;
+//         for (int i = b; i > 3; i--)
+//         {
+//             c = c * 10;
+//         }
+
+//         result = (a / c) % 10;
+//     }
+// return result;
+// }
 
 
 
--- Task_HomeWork_015/Program.cs
+++ Task_HomeWork_015/Program.cs
@@ -6,13 +6,71 @@
 // 7 -> да
 // 1 -> нет
 
-Console.WriteLine("Введите число от 1 до 7: ");
-int dayNumber = int.Parse(Console.ReadLine()?? "0");
-if (dayNumber > 0 && dayNumber < 6)
+// Console.WriteLine("Введите число от 1 до 7: ");
+// int dayNumber = int.Parse(Console.ReadLine()?? "0");
+// if (dayNumber > 0 && dayNumber < 6)
+// {
+//     Console.WriteLine("Иди, работай пожалуйста  ");
+// }
+// else
+// {
+//     Console.WriteLine("Можешь отдохнуть  ");
+// }
+
+// Задача 15: Напишите программу, которая принимает 
+// на вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+// Console.Clear();
+
+// Console.Write("Введите число ");
+// int numberDay = Convert.ToInt32(Console.ReadLine());
+
+// if (numberDay > 5)
+// {
+//     Console.Write("Иди отдыхай");
+// }
+// else
+// {
+//     Console.Write("Иди работай");
+// }
+
+
+// Второе решение 
+Console.Clear();
+
+Console.Write("Введите число дня недели ");
+int numberDay = Convert.ToInt32(Console.ReadLine());
+
+if (numberDay == 1)
 {
-    Console.WriteLine("Иди, работай пожалуйста  ");
+    Console.Write("Иди работай, понедельник");
 }
-else
+if (numberDay == 2)
 {
-    Console.WriteLine("Можешь отдохнуть  ");
+    Console.Write("Иди работай, вторник");
+}
+if (numberDay == 3)
+{
+    Console.Write("Иди работай, среда");
+}
+if (numberDay == 4)
+{
+    Console.Write("Иди работай, четверг");
+}
+if (numberDay == 5)
+{
+    Console.Write("Иди работай, пятница");
+}
+if (numberDay == 6)
+{
+    Console.Write("Иди отдыхай, суббота");
+}
+if (numberDay == 7)
+{
+    Console.Write("Иди отдыхай, воскресенье");
+}
+if (numberDay >=8)
+{
+    Console.Write("Такого дня недели нет ");
 }
