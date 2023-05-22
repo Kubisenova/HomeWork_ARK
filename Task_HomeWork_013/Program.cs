9ae8f6e4b6fbfbe80da8494f9b36c4e6dfb93427
Aygul <Your>
2023-05-22 17:53:49 +0300

    Task_HomeWork_013

--- Task_HomeWork_013/Program.cs
+++ Task_HomeWork_013/Program.cs
@@ -36,20 +36,20 @@
 
 
 
-// // // Второе решение
-Console.Clear();
-Console.WriteLine("Введите число:  ");
-string? numA = Console.ReadLine();
+// // // // Второе решение
+// Console.Clear();
+// Console.WriteLine("Введите число:  ");
+// string? numA = Console.ReadLine();
 
-if (numA.Length >= 3)
-{
-    char[] array = numA.ToCharArray();
-    Console.WriteLine(array[2]);
-}
-else
-{
-    Console.WriteLine("Третьей цифры нет ");
-}
+// if (numA.Length >= 3)
+// {
+//     char[] array = numA.ToCharArray();
+//     Console.WriteLine(array[2]);
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет ");
+// }
 
 
 // Задача 13: Напишите программу, которая выводит третью
@@ -69,3 +69,44 @@ else
 // {
 //     Console.WriteLine(" Здесь нет третьей цифры");
 // }
+
+
+
+// Решение 3
+
+Console.Clear();
+
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+
+
+// ФУНКЦИИ------------------------------------------------------------------------------------------------------
+
+// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция принимает число введенное пользователем, количество символов, и выводит третью цифру числа. Если 3 цифры нет, сообщает и выводит 0.
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
