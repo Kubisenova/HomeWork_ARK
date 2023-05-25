50a97dadb3bb59f69c4c73dca41147e70f836dad
Aygul <Your>
2023-05-25 20:58:40 +0300

    Task_HomeWork_023

--- Task_HomeWork_023/Program.cs
+++ Task_HomeWork_023/Program.cs
@@ -5,15 +5,35 @@
 // 5 -> 1, 8, 27, 64, 125
 
 
-int number = ReadInt("Введите число N: ");
 
-for (int i = 1; i <= number; i++)
-{ 
-    Console.Write($"{i*i*i} ");
-}
-// Функция ввода сообщения
-int ReadInt(string message)
+// // Первое решение 
+
+// int number = ReadInt("Введите число N: ");
+
+// for (int i = 1; i <= number; i++)
+// { 
+//     Console.Write($"{i*i*i} ");
+// }
+// // Функция ввода сообщения
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+
+// Второе решение 
+
+Console.Write("Введите число N: ");
+string text_N = Console.ReadLine();
+double N = Convert.ToInt32(text_N);
+double x = 0;
+double y = 0;
+while (x <= N)
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+    y = Math.Pow(x, 3);
+    Console.Write("   " + y );
+    x++;
 }
